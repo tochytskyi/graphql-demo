7630b933eee6471217a8a0727339b8ec2c1e3f42
Volodymyr Tochytskyi <tchtsk@macpaw.com>
2023-06-18 17:30:44 +0300

    Init

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	database "github.com/tchtsk/graphql-demo/internal/pkg/db/mysql"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/tchtsk/graphql-demo/graph"
+	"github.com/tchtsk/graphql-demo/internal/auth"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
+
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
